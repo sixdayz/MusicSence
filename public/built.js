83d357bde710aed3eda711af69c6f1ad6e122288
Andrey Goryachev <andrey@sixdays.ru>
2014-09-02 22:38:02 +0400

    Метод генерации ленты

--- public/built.js
+++ public/built.js
@@ -24259,11 +24259,25 @@ App.Managers.FeedManager = Backbone.Model.extend({
         this.set('context_manager', options.context_manager);
     },
 
-    generate: function(query, type, context) {
+    generate: function(query, type) {
+        var deferred = new $.Deferred();
 
+        this.get('context_manager').createContext().done(function(context) {
+            this.get('api_client').post('/musicfeed/generate', {
+                q:          query,
+                type:       type,
+                context:    JSON.stringify(context.toJSON())
+            }).done(function(response) {
+
+                // Вернем обещанный ответ
+                deferred.resolve(response);
+            });
+        }.bind(this));
+
+        return deferred.promise();
     },
 
-    getSongs: function(feedId, limit, context) {
+    getSongs: function(feedId, limit) {
 
     },
 
